d770ed374ea1201447d6723ffe83a02ad06c3792
Lellansin <lellansin@gmail.com>
2017-07-10 15:45:34 +0800

    Fix encoding problem

--- conf.py
+++ conf.py
@@ -39,7 +39,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The encoding of source files.
-source_encoding = 'utf-8-sig'
+#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -206,6 +206,7 @@ htmlhelp_basename = 'PostgRESTdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
+
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 #'papersize': 'letterpaper',
@@ -218,6 +219,23 @@ latex_elements = {
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
+
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
